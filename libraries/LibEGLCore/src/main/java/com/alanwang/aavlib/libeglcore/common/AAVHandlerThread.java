41cf0e1220cc29d8548448539071996530519517
alanwang4523 <alanwang4523@gmail.com>
2019-01-26 23:57:41 +0800

    AAVHandlerThread  增加退出处理

--- libraries/LibEGLCore/src/main/java/com/alanwang/aavlib/libeglcore/common/AAVHandlerThread.java
+++ libraries/LibEGLCore/src/main/java/com/alanwang/aavlib/libeglcore/common/AAVHandlerThread.java
@@ -23,6 +23,7 @@ public class AAVHandlerThread {
         void handleMsg(Message msg);
     }
 
+    private static final int MSG_QUIT = -402181;
     private Handler mHandler;
     private final HandlerThread mHandlerThread;
 
@@ -30,11 +31,22 @@ public class AAVHandlerThread {
         mHandlerThread = new HandlerThread(name);
     }
 
+    /**
+     * 启动线程
+     * @param callback
+     */
     public void start(IMsgCallback callback) {
         if (mHandler == null) {
             mHandlerThread.start();
             mHandler = new InternalHandler(callback, mHandlerThread.getLooper());
         }
+    }
+
+    /**
+     * 停止线程
+     */
+    public void stop() {
+        mHandler.sendEmptyMessage(MSG_QUIT);
     }
 
     private static class InternalHandler extends Handler {
@@ -47,6 +59,13 @@ public class AAVHandlerThread {
 
         @Override
         public void handleMessage(Message msg) {
+            if (msg.what == MSG_QUIT) {
+                Looper looper = Looper.myLooper();
+                if (looper != null) {
+                    looper.quit();
+                }
+                return;
+            }
             if (mMsgCallback != null) {
                 mMsgCallback.handleMsg(msg);
             }
