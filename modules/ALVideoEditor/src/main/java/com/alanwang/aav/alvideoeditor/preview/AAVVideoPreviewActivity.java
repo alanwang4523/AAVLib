d28582f393b6c197d0bc159937c495f276d42d8c
alanwang4523 <alanwang4523@gmail.com>
2019-01-29 01:00:37 +0800

    资源复用

---
+++ modules/ALVideoEditor/src/main/java/com/alanwang/aav/alvideoeditor/preview/AAVVideoPreviewActivity.java
@@ -0,0 +1,21 @@
+package com.alanwang.aav.alvideoeditor.preview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.alanwang.aav.alvideoeditor.R;
+
+/**
+ * Author: AlanWang4523.
+ * Date: 19/1/29 00:47.
+ * Mail: alanwang4523@gmail.com
+ */
+
+public class AAVVideoPreviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aav_activity_video_preview);
+    }
+}
