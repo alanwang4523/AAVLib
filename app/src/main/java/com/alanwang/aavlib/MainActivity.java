04e49443861f8e32c06e4fc10c731bff5c48e9f2
alanwang4523 <alanwang4523@gmail.com>
2019-01-29 00:10:05 +0800

    增加主测试界面

--- app/src/main/java/com/alanwang/aavlib/MainActivity.java
+++ app/src/main/java/com/alanwang/aavlib/MainActivity.java
@@ -2,12 +2,22 @@ package com.alanwang.aavlib;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_goto_video_editor:
+                break;
+            default:
+        }
     }
 }
