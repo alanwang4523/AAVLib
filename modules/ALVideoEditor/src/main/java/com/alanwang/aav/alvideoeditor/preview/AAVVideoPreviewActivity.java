5d4d239c6d77d7366d29aaca9de0c104cf561c71
alanwang4523 <alanwang4523@gmail.com>
2019-01-30 00:03:38 +0800

    增加视频预览的基本界面

--- modules/ALVideoEditor/src/main/java/com/alanwang/aav/alvideoeditor/preview/AAVVideoPreviewActivity.java
+++ modules/ALVideoEditor/src/main/java/com/alanwang/aav/alvideoeditor/preview/AAVVideoPreviewActivity.java
@@ -2,8 +2,12 @@ package com.alanwang.aav.alvideoeditor.preview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.alanwang.aav.alvideoeditor.R;
+import com.alanwang.aavlib.libvideo.surface.AAVSurfaceView;
+import com.alanwang.aavlib.libvideo.surface.ISurfaceCallback;
 
 /**
  * Author: AlanWang4523.
@@ -11,11 +15,30 @@ import com.alanwang.aav.alvideoeditor.R;
  * Mail: alanwang4523@gmail.com
  */
 
-public class AAVVideoPreviewActivity extends AppCompatActivity {
+public class AAVVideoPreviewActivity extends AppCompatActivity implements ISurfaceCallback{
+
+    private AAVSurfaceView mAAVSurfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //取消状态栏
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.aav_activity_video_preview);
+
+        mAAVSurfaceView = findViewById(R.id.video_surface_view);
+        mAAVSurfaceView.setSurfaceCallback(this);
+    }
+
+    @Override
+    public void onSurfaceChanged(Object surface, int w, int h) {
+        
+    }
+
+    @Override
+    public void onSurfaceDestroyed(Object surface) {
+
     }
 }
