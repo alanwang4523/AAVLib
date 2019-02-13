72c938ceca4d7315dc6ca6233a2f53df04d821bd
alanwang4523 <alanwang4523@gmail.com>
2019-02-14 00:41:53 +0800

    增加动态权限申请

--- app/src/main/java/com/alanwang/aavlib/MainActivity.java
+++ app/src/main/java/com/alanwang/aavlib/MainActivity.java
@@ -1,12 +1,13 @@
 package com.alanwang.aavlib;
 
+import android.Manifest;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
 import com.alanwang.aav.alvideoeditor.preview.AAVVideoPreviewActivity;
+import com.alanwang.aavlib.libutils.RuntimePermissionsHelper;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -16,6 +17,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RuntimePermissionsHelper runtimePermissionsHelper = RuntimePermissionsHelper.create(this, null,
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (!runtimePermissionsHelper.allPermissionsGranted()) {
+            runtimePermissionsHelper.makeRequest();
+        }
 
         btn_goto_video_editor = findViewById(R.id.btn_goto_video_editor);
         btn_goto_video_editor.setOnClickListener(this);
