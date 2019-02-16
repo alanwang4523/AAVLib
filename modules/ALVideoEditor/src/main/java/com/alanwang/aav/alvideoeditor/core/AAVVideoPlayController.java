c2b6da4b1522a1522fb21914089d17d901df3bff
alanwang4523 <alanwang4523@gmail.com>
2019-02-16 23:40:31 +0800

    创建 AAVVideoPlayController

---
+++ modules/ALVideoEditor/src/main/java/com/alanwang/aav/alvideoeditor/core/AAVVideoPlayController.java
@@ -0,0 +1,18 @@
+package com.alanwang.aav.alvideoeditor.core;
+
+import com.alanwang.aavlib.libeglcore.common.AAVSurfaceTexture;
+import com.alanwang.aavlib.libeglcore.engine.AAVMainGLEngine;
+import com.alanwang.aavlib.libvideo.player.IVideoPlayer;
+
+/**
+ * Author: AlanWang4523.
+ * Date: 19/2/16 21:15.
+ * Mail: alanwang4523@gmail.com
+ */
+
+public class AAVVideoPlayController {
+
+    private IVideoPlayer mVideoPlayer;
+    private AAVSurfaceTexture mAAVSurface;
+    private AAVMainGLEngine mMainGLEngine;
+}
