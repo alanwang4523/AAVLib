d488f2a6b408dcb6dbb610079df1a62e7a46b152
alanwang4523 <alanwang4523@gmail.com>
2019-02-17 00:15:37 +0800

    update AAVVideoPlayController

--- modules/ALVideoEditor/src/main/java/com/alanwang/aav/alvideoeditor/core/AAVVideoPlayController.java
+++ modules/ALVideoEditor/src/main/java/com/alanwang/aav/alvideoeditor/core/AAVVideoPlayController.java
@@ -16,19 +16,42 @@ import com.alanwang.aavlib.libvideo.player.IVideoPlayer;
  * Mail: alanwang4523@gmail.com
  */
 
-public class AAVVideoPlayController {
+public class AAVVideoPlayController implements
+        IVideoPlayer.OnPlayReadyListener,
+        IGLEngineCallback,
+        AAVFrameAvailableListener {
+
+    public interface IControllerCallback {
+        /**
+         * 视频大小确定回调
+         * @param width
+         * @param height
+         */
+        void onVideoSizeChanged(int width, int height);
+    }
 
     private IVideoPlayer mVideoPlayer;
     private AAVSurfaceTexture mAAVSurface;
     private AAVMainGLEngine mMainGLEngine;
+    private IControllerCallback iControllerCallback;
 
 
     public AAVVideoPlayController() {
         mVideoPlayer = new AAVVideoPlayer();
-        mAAVSurface = new AAVSurfaceTexture();
-        mAAVSurface.setFrameAvailableListener(mAAVFrameAvailableListener);
-        mMainGLEngine = new AAVMainGLEngine(mGLEngineCallback);
+        mVideoPlayer.setOnPlayReadyListener(this);
+        mMainGLEngine = new AAVMainGLEngine(this);
         mMainGLEngine.start();
+    }
+
+    public void setiControllerCallback(IControllerCallback iControllerCallback) {
+        this.iControllerCallback = iControllerCallback;
+    }
+
+    public void setVideoPath(String videoPath) {
+        if (mVideoPlayer.isPlaying()) {
+            mVideoPlayer.stop();
+        }
+        mVideoPlayer.preparePlayer(videoPath);
     }
 
     public void updateSurface(Surface surface, int w, int h) {
@@ -39,37 +62,42 @@ public class AAVVideoPlayController {
         mMainGLEngine.destroySurface();
     }
 
-    private IGLEngineCallback mGLEngineCallback = new IGLEngineCallback() {
-        @Override
-        public void onEngineStart() {
-
+    @Override
+    public void onPlayReady(int width, int height) {
+        if (iControllerCallback != null) {
+            iControllerCallback.onVideoSizeChanged(width, height);
         }
+    }
 
-        @Override
-        public void onSurfaceUpdate(Surface surface, int width, int height) {
+    @Override
+    public void onEngineStart() {
+        mAAVSurface = new AAVSurfaceTexture();
+        mAAVSurface.setFrameAvailableListener(this);
+        mVideoPlayer.setSurface(mAAVSurface.getSurface());
+    }
 
-        }
+    @Override
+    public void onSurfaceUpdate(Surface surface, int width, int height) {
 
-        @Override
-        public void onRender(AAVMessage msg) {
+    }
 
-        }
+    @Override
+    public void onRender(AAVMessage msg) {
 
-        @Override
-        public void onSurfaceDestroy() {
+    }
 
-        }
+    @Override
+    public void onSurfaceDestroy() {
 
-        @Override
-        public void onEngineRelease() {
+    }
 
-        }
-    };
+    @Override
+    public void onEngineRelease() {
 
-    private AAVFrameAvailableListener mAAVFrameAvailableListener = new AAVFrameAvailableListener() {
-        @Override
-        public void onFrameAvailable(AAVSurfaceTexture surfaceTexture) {
+    }
 
-        }
-    };
+    @Override
+    public void onFrameAvailable(AAVSurfaceTexture surfaceTexture) {
+
+    }
 }
