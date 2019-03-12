c1b7e31819b44fa64220917cbf69db79e357c3ee
alanwang4523 <alanwang4523@gmail.com>
2019-03-13 01:20:17 +0800

    增加 LibAudio 的测试界面

---
+++ app/src/main/java/com/alanwang/aavlib/TestLibAudioActivity.java
@@ -0,0 +1,40 @@
+package com.alanwang.aavlib;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * Author: AlanWang4523.
+ * Date: 19/3/13 01:13.
+ * Mail: alanwang4523@gmail.com
+ */
+public class TestLibAudioActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_libmediacore);
+
+        TextView btn_test_record_to_wav = findViewById(R.id.btn_libaudio_record_to_wav);
+        btn_test_record_to_wav.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_libmediacore_test_extract_audio:
+                testRecordAudioToWav();
+                break;
+            default:
+        }
+    }
+
+    /**
+     * 测试录制音频到 wav
+     */
+    private void testRecordAudioToWav() {
+
+    }
+}
