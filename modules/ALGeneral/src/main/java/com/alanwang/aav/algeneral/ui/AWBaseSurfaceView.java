945e79a8386f3f0b2083504a9392a6a86081aec9
alanwang4523 <alanwang4523@gmail.com>
2019-03-24 21:23:29 +0800

    完成 AWBaseSurfaceView

---
+++ modules/ALGeneral/src/main/java/com/alanwang/aav/algeneral/ui/AWBaseSurfaceView.java
@@ -0,0 +1,102 @@
+package com.alanwang.aav.algeneral.ui;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+/**
+ * Author: AlanWang4523.
+ * Date: 19/3/24 20:22.
+ * Mail: alanwang4523@gmail.com
+ */
+public abstract class AWBaseSurfaceView extends SurfaceView implements SurfaceHolder.Callback, Runnable {
+
+    protected static int FRAME_INTERVAL_TIME = 30;
+    protected Thread workThread;
+    protected boolean isRun = true;
+    protected SurfaceHolder surfaceHolder;
+    protected Canvas canvas;
+    protected Paint defaultPaint;
+
+    public AWBaseSurfaceView(Context context) {
+        super(context);
+        initView(null);
+    }
+
+    public AWBaseSurfaceView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView(attrs);
+    }
+
+    protected void initView(AttributeSet attrs){
+        setKeepScreenOn(true);
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+        surfaceHolder = this.getHolder();
+        surfaceHolder.addCallback(this);
+        defaultPaint = new Paint();
+        defaultPaint.setAntiAlias(true);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        isRun = true;
+        workThread = new Thread(this);
+        workThread.start();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        isRun = false;
+    }
+
+    @Override
+    public void run() {
+        while(isRun){
+            long startTime = System.currentTimeMillis();
+            handleLogic();
+            myDraw();
+            long endTime = System.currentTimeMillis();
+            try {
+                if(endTime - startTime < FRAME_INTERVAL_TIME){
+                    Thread.sleep(FRAME_INTERVAL_TIME - (endTime - startTime));
+                }
+            } catch (Exception ex) {
+            }
+        }
+    }
+
+    private void myDraw(){
+        try {
+            canvas = surfaceHolder.lockCanvas();
+            if(canvas != null) {
+                justToDraw(canvas);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if(canvas != null){
+                surfaceHolder.unlockCanvasAndPost(canvas);
+            }
+        }
+    }
+
+    /**
+     * 具体的绘制
+     * @param canvas
+     */
+    protected abstract void justToDraw(Canvas canvas);
+
+    /**
+     * 计算、处理逻辑
+     */
+    protected abstract void handleLogic();
+}
