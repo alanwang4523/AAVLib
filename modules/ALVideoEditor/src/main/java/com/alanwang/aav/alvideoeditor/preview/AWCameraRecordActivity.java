00a23654982e57fcfb8bbaaac9d309ea9e5e7dc3
alanwang4523 <alanwang4523@gmail.com>
2019-04-01 02:11:13 +0800

    完善录制界面

--- modules/ALVideoEditor/src/main/java/com/alanwang/aav/alvideoeditor/preview/AWCameraRecordActivity.java
+++ modules/ALVideoEditor/src/main/java/com/alanwang/aav/alvideoeditor/preview/AWCameraRecordActivity.java
@@ -3,8 +3,11 @@ package com.alanwang.aav.alvideoeditor.preview;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Surface;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.alanwang.aav.algeneral.ui.AWRecordButton;
 import com.alanwang.aav.algeneral.ui.EnhancedRelativeLayout;
@@ -17,11 +20,15 @@ import com.alanwang.aavlib.libvideo.surface.ISurfaceCallback;
  * Date: 19/4/1 00:26.
  * Mail: alanwang4523@gmail.com
  */
-public class AWCameraRecordActivity extends AppCompatActivity implements ISurfaceCallback {
+public class AWCameraRecordActivity extends AppCompatActivity
+        implements ISurfaceCallback, View.OnClickListener {
 
     private EnhancedRelativeLayout mVideoLayout;
     private AWSurfaceView mAWSurfaceView;
     private AWRecordButton btnRecord;
+    private ImageView btnClose;
+    private ImageView btnCameraSwitchcover;
+    private ImageView btnFlashlightSwitchcover;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +43,16 @@ public class AWCameraRecordActivity extends AppCompatActivity implements ISurfac
 
         mAWSurfaceView = findViewById(R.id.video_surface_view);
         mAWSurfaceView.setSurfaceCallback(this);
+
+        btnClose = findViewById(R.id.iv_btn_close);
+        btnClose.setOnClickListener(this);
+
+        btnFlashlightSwitchcover = findViewById(R.id.iv_btn_flashlight_switchover);
+        btnFlashlightSwitchcover.setOnClickListener(this);
+        btnFlashlightSwitchcover.setSelected(false);
+
+        btnCameraSwitchcover = findViewById(R.id.iv_btn_camera_switchover);
+        btnCameraSwitchcover.setOnClickListener(this);
 
         btnRecord = findViewById(R.id.btn_camera_record);
         btnRecord.setRecordListener(new AWRecordButton.OnRecordListener() {
@@ -58,5 +75,20 @@ public class AWCameraRecordActivity extends AppCompatActivity implements ISurfac
     @Override
     public void onSurfaceDestroyed(Surface surface) {
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.iv_btn_close) {
+            finish();
+        } else if (v.getId() == R.id.iv_btn_flashlight_switchover) {
+            if (btnFlashlightSwitchcover.isSelected()) {
+                btnFlashlightSwitchcover.setSelected(false);
+            } else {
+                btnFlashlightSwitchcover.setSelected(true);
+            }
+        } else if (v.getId() == R.id.iv_btn_camera_switchover) {
+
+        }
     }
 }
