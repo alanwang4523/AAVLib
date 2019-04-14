65eb9ecab6b6b6dd673e500c97d326b2991cfab0
alanwang4523 <alanwang4523@gmail.com>
2019-04-14 19:58:44 +0800

    增加自定义进度条控件

---
+++ modules/ALGeneral/src/main/java/com/alanwang/aav/algeneral/ui/AWLoadingDialog.java
@@ -0,0 +1,116 @@
+package com.alanwang.aav.algeneral.ui;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import com.alanwang.aav.algeneral.R;
+
+/**
+ * Author: AlanWang4523.
+ * Date: 19/4/14 19:42.
+ * Mail: alanwang4523@gmail.com
+ */
+public class AWLoadingDialog extends Dialog {
+    
+    protected TextView tvHint;
+    protected ProgressBar mLoadingPB;
+    protected Context mContext;
+
+    private String mHintText;
+    private boolean isFullScreen;
+
+    public AWLoadingDialog(Context context) {
+        this(context, false);
+    }
+
+    public AWLoadingDialog(Context context, boolean isFullScreen) {
+        super(context, R.style.dialog);
+        this.isFullScreen = isFullScreen;
+        this.mContext = context;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setFullScreenStatus();
+        setContentView(R.layout.aw_loading_dialog);
+        mLoadingPB = findViewById(R.id.aw_loading_view);
+        tvHint = findViewById(R.id.tv_loading_hint);
+
+    }
+
+    public void setLoadingText(String text) {
+        mHintText = text;
+    }
+
+    private void setFullScreenStatus() {
+        if (isFullScreen) {
+            requestWindowFeature(Window.FEATURE_NO_TITLE);
+            if (null != getWindow()) {
+                getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            }
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            setTranslucentStatus();  //需要注意页面会盖在通知栏上,各页面可以增加paddingTop 24dp解决
+        }
+    }
+
+    @TargetApi(19)
+    private void setTranslucentStatus() {
+        Window win = getWindow();
+        if (null != win) {
+            WindowManager.LayoutParams winParams = win.getAttributes();
+            final int bits = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+            winParams.flags |= bits;
+            win.setAttributes(winParams);
+        }
+    }
+
+    @Override
+    public void show() {
+        if (mContext == null) {
+            return;
+        } else if (mContext instanceof Activity) {
+            Activity activity = (Activity) mContext;
+            if (activity.isDestroyed()) {
+                return;
+            }
+        }
+        try {
+            super.show();
+            if (!TextUtils.isEmpty(mHintText)) {
+                tvHint.setVisibility(View.VISIBLE);
+                tvHint.setText(mHintText);
+            } else {
+                tvHint.setVisibility(View.GONE);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void dismiss() {
+        if (mContext == null) {
+            return;
+        } else if (mContext instanceof Activity) {
+            Activity activity = (Activity) mContext;
+            if (activity.isDestroyed()) {
+                return;
+            }
+        }
+        try {
+            super.dismiss();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
