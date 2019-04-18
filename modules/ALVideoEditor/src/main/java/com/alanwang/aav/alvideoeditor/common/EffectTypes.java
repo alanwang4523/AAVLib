f9baf8b9e0f47f761ab92b8879f9a2e102763f0b
alanwang4523 <alanwang4523@gmail.com>
2019-04-19 07:52:57 +0800

    增加 Style 滤镜资源

---
+++ modules/ALVideoEditor/src/main/java/com/alanwang/aav/alvideoeditor/common/EffectTypes.java
@@ -0,0 +1,19 @@
+package com.alanwang.aav.alvideoeditor.common;
+
+/**
+ * Author: AlanWang4523.
+ * Date: 19/4/19 07:34.
+ * Mail: alanwang4523@gmail.com
+ */
+public class EffectTypes {
+
+    public static final int STYLE_TYPE_ORIGINAL      = 1001; // 原图
+    public static final int STYLE_TYPE_ROMANTIC      = 1002; // 浪漫
+    public static final int STYLE_TYPE_FRESH         = 1003; // 清新
+    public static final int STYLE_TYPE_BEAUTIFUL     = 1004; // 唯美
+    public static final int STYLE_TYPE_PINK          = 1005; // 粉嫩
+    public static final int STYLE_TYPE_REMINISCENCE  = 1006; // 怀旧
+    public static final int STYLE_TYPE_BLUES         = 1007; // 蓝调
+    public static final int STYLE_TYPE_COOL          = 1008; // 清凉
+    public static final int STYLE_TYPE_JAPANESE      = 1009; // 日系
+}
--- modules/ALVideoEditor/src/main/java/com/alanwang/aav/alvideoeditor/ui/StyleEffectView.java
+++ modules/ALVideoEditor/src/main/java/com/alanwang/aav/alvideoeditor/ui/StyleEffectView.java
@@ -8,13 +8,19 @@ import android.view.LayoutInflater;
 import android.widget.RelativeLayout;
 
 import com.alanwang.aav.alvideoeditor.R;
+import com.alanwang.aav.alvideoeditor.beans.EffectBean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Author: AlanWang4523.
  * Date: 19/4/18 08:49.
  * Mail: alanwang4523@gmail.com
  */
-public class StyleEffectView extends RelativeLayout {
+public class StyleEffectView extends RelativeLayout implements EffectSelectListener {
+
+    private StyleEffectAdapter effectAdapter;
 
     public StyleEffectView(Context context) {
         super(context);
@@ -37,5 +43,21 @@ public class StyleEffectView extends RelativeLayout {
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setLayoutManager(new LinearLayoutManager(this.getContext(), LinearLayoutManager.HORIZONTAL, false));
 
+        effectAdapter = new StyleEffectAdapter(getContext());
+        effectAdapter.setEffectSelectListener(this);
+        effectAdapter.setItemList(getEffectList());
+
+        recyclerView.setAdapter(effectAdapter);
+    }
+
+    @Override
+    public void onEffectSelect(int type) {
+
+    }
+
+    private List<EffectBean> getEffectList() {
+        List<EffectBean> effectList = new ArrayList<>();
+
+        return effectList;
     }
 }
