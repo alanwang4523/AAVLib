9665aeb77a87dc1b10afdda61f36beaed525ad4b
alanwang4523 <alanwang4523@gmail.com>
2019-04-19 08:22:25 +0800

    资源压缩更新

--- modules/ALVideoEditor/src/main/java/com/alanwang/aav/alvideoeditor/ui/StyleEffectView.java
+++ modules/ALVideoEditor/src/main/java/com/alanwang/aav/alvideoeditor/ui/StyleEffectView.java
@@ -6,10 +6,9 @@ import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.RelativeLayout;
-
 import com.alanwang.aav.alvideoeditor.R;
 import com.alanwang.aav.alvideoeditor.beans.EffectBean;
-
+import com.alanwang.aav.alvideoeditor.common.EffectTypes;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -58,6 +57,46 @@ public class StyleEffectView extends RelativeLayout implements EffectSelectListe
     private List<EffectBean> getEffectList() {
         List<EffectBean> effectList = new ArrayList<>();
 
+        effectList.add(new EffectBean(
+                EffectTypes.STYLE_TYPE_ORIGINAL,
+                getText(R.string.lib_video_editor_video_effect_name_original),
+                R.drawable.icon_style_orginal));
+        effectList.add(new EffectBean(
+                EffectTypes.STYLE_TYPE_ROMANTIC,
+                getText(R.string.lib_video_editor_video_effect_name_romantic),
+                R.drawable.icon_style_romantic));
+        effectList.add(new EffectBean(
+                EffectTypes.STYLE_TYPE_FRESH,
+                getText(R.string.lib_video_editor_video_effect_name_fresh),
+                R.drawable.icon_style_fresh));
+        effectList.add(new EffectBean(
+                EffectTypes.STYLE_TYPE_BEAUTIFUL,
+                getText(R.string.lib_video_editor_video_effect_name_beautiful),
+                R.drawable.icon_style_beautiful));
+        effectList.add(new EffectBean(
+                EffectTypes.STYLE_TYPE_PINK,
+                getText(R.string.lib_video_editor_video_effect_name_pink),
+                R.drawable.icon_style_pink));
+        effectList.add(new EffectBean(
+                EffectTypes.STYLE_TYPE_REMINISCENCE,
+                getText(R.string.lib_video_editor_video_effect_name_reminiscence),
+                R.drawable.icon_style_reminiscence));
+        effectList.add(new EffectBean(
+                EffectTypes.STYLE_TYPE_BLUES,
+                getText(R.string.lib_video_editor_video_effect_name_blues),
+                R.drawable.icon_style_blues));
+        effectList.add(new EffectBean(
+                EffectTypes.STYLE_TYPE_COOL,
+                getText(R.string.lib_video_editor_video_effect_name_cool),
+                R.drawable.icon_style_cool));
+        effectList.add(new EffectBean(
+                EffectTypes.STYLE_TYPE_JAPANESE,
+                getText(R.string.lib_video_editor_video_effect_name_japanese),
+                R.drawable.icon_style_japanese));
         return effectList;
+    }
+
+    private String getText(int textId) {
+        return getContext().getResources().getString(textId);
     }
 }
