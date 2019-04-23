c4023fcb6c61cb98a68f3f299768350963eb913a
alanwang4523 <alanwang4523@gmail.com>
2019-04-23 23:51:17 +0800

    LibEGLCore 增加 Apache License

--- libraries/LibEGLCore/src/main/java/com/alanwang/aavlib/libeglcore/egl/AWWindowSurface.java
+++ libraries/LibEGLCore/src/main/java/com/alanwang/aavlib/libeglcore/egl/AWWindowSurface.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2019-present, AlanWang4523 (alanwang4523@gmail.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.alanwang.aavlib.libeglcore.egl;
 
 import android.opengl.EGL14;
@@ -15,7 +30,6 @@ public class AWWindowSurface {
 
     public void createWindowSurface(EGLCore eglCore, Object surface) {
         if (mEGLSurface != EGL14.EGL_NO_SURFACE) {
-//            throw new IllegalStateException("surface already created");
             eglCore.makeNothingCurrent();
             eglCore.destroyEGLSurface(mEGLSurface);
             mEGLSurface = EGL14.EGL_NO_SURFACE;
